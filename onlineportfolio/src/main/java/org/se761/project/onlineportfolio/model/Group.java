91b29dcdeea4a6949aacd7dc39aa8ea8801070fd
dgop507 <dgop507@aucklanduni.ac.nz>
2015-08-13 20:18:39 +1200

    Added Group class

--- onlineportfolio/src/main/java/org/se761/project/onlineportfolio/model/Group.java
+++ onlineportfolio/src/main/java/org/se761/project/onlineportfolio/model/Group.java
@@ -1,5 +1,45 @@
 package org.se761.project.onlineportfolio.model;
 
 public class Group {
+	
+	private int groupId;
+	private AdminGroup adminGroup;
+	private Account account;
+	
+	public Group(){
+		
+	}
 
+	public Group(int groupId, AdminGroup adminGroup, Account account) {
+		super();
+		this.groupId = groupId;
+		this.adminGroup = adminGroup;
+		this.account = account;
+	}
+
+	public int getGroupId() {
+		return groupId;
+	}
+
+	public void setGroupId(int groupId) {
+		this.groupId = groupId;
+	}
+
+	public AdminGroup getAdminGroup() {
+		return adminGroup;
+	}
+
+	public void setAdminGroup(AdminGroup adminGroup) {
+		this.adminGroup = adminGroup;
+	}
+
+	public Account getAccount() {
+		return account;
+	}
+
+	public void setAccount(Account account) {
+		this.account = account;
+	}
+
+	
 }
