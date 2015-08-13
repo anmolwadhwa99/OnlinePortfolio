510c11328b98e604f9dd44821ce5990d6aa30319
dgop507 <dgop507@aucklanduni.ac.nz>
2015-08-13 22:32:59 +1200

    Implemented AdminGroupResource

--- onlineportfolio/src/main/java/org/se761/project/onlineportfolio/resource/AdminGroupResource.java
+++ onlineportfolio/src/main/java/org/se761/project/onlineportfolio/resource/AdminGroupResource.java
@@ -1,5 +1,46 @@
 package org.se761.project.onlineportfolio.resource;
 
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import org.se761.project.onlineportfolio.model.Account;
+import org.se761.project.onlineportfolio.model.AdminGroup;
+
+@Path("/admin")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
 public class AdminGroupResource {
+	
+	/**
+	 * Adding a admin group
+	 */
+	
+	@POST
+	public AdminGroup addAdminGroup(AdminGroup adminGroup){
+		return null;
+	}
+	
+	/**
+	 * Getting a admin group
+	 */
+	@GET
+	@Path("/{adminGroupId}")
+	public AdminGroup getAdminGroup(int adminGroupId){
+		return null;
+	}
+	
+	/**
+	 * Delete admin group
+	 */
+	@DELETE
+	@Path("/{adminGroupId}")
+	public AdminGroup deleteAccount(int adminGroupId){
+		return null;
+	}
 
 }
