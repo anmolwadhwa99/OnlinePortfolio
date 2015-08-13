b4dd2011dde246cf7a2215136cbba6a9e6c0f785
dgop507 <dgop507@aucklanduni.ac.nz>
2015-08-13 21:45:06 +1200

    Implemented AccountResource

--- onlineportfolio/src/main/java/org/se761/project/onlineportfolio/resource/AccountResource.java
+++ onlineportfolio/src/main/java/org/se761/project/onlineportfolio/resource/AccountResource.java
@@ -1,5 +1,78 @@
 package org.se761.project.onlineportfolio.resource;
 
+import java.util.List;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import org.se761.project.onlineportfolio.model.Account;
+
+
+@Path("/account")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
 public class AccountResource {
+	
+	/**
+	 * Adding an account
+	 */
+	
+	@POST
+	public Account addAccount(Account account){
+		return null;
+	}
+	
+	/**
+	 * Getting any account
+	 */
+	@GET
+	@Path("/{accountId}")
+	public Account getAccount(int accountId){
+		return null;
+	}
+	
+	/**
+	 * Getting all accounts
+	 */
+	@GET
+	public List<Account> getAllAccounts(){
+		return null;
+	}
+	
+	/**
+	 * Getting all admin accounts
+	 */
+	@GET
+	@Path("/{admin}")
+	public List<Account> getAdminAccounts(){
+		return null;
+	}
+	
+	/**
+	 * Getting all client accounts
+	 */
+	@GET
+	@Path("/{client}")
+	public List<Account> getClientAccounts(){
+		return null;
+	}
+	
+	/**
+	 * Delete an account
+	 */
+	@DELETE
+	@Path("/{accountId}")
+	public Account deleteAccount(int accountId){
+		return null;
+	}
+	
+	
+	
+	
 
 }
