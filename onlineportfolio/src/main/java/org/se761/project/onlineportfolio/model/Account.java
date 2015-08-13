0c4e965218ab808b20bee18b015ee43f382cd8fb
dgop507 <dgop507@aucklanduni.ac.nz>
2015-08-13 18:51:36 +1200

    Added Account class

--- onlineportfolio/src/main/java/org/se761/project/onlineportfolio/model/Account.java
+++ onlineportfolio/src/main/java/org/se761/project/onlineportfolio/model/Account.java
@@ -1,5 +1,49 @@
 package org.se761.project.onlineportfolio.model;
 
 public class Account {
+	
+	private int accountId;
+	private String userName;
+	private String pin;
+	private boolean isAdmin;
+	
+	public Account(){
+		
+	}
+	
+	public Account(int accountId, String userName, String pin, boolean isAdmin) {
+		super();
+		this.accountId = accountId;
+		this.userName = userName;
+		this.pin = pin;
+		this.isAdmin = isAdmin;
+	}
+	
+	public int getAccountId() {
+		return accountId;
+	}
+	public void setAccountId(int accountId) {
+		this.accountId = accountId;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getPin() {
+		return pin;
+	}
+	public void setPin(String pin) {
+		this.pin = pin;
+	}
+	public boolean isAdmin() {
+		return isAdmin;
+	}
+	public void setAdmin(boolean isAdmin) {
+		this.isAdmin = isAdmin;
+	}
+	
+	
 
 }
