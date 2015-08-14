bfdaf583b98b0f07329d5f31767d20c9d1d54b7f
dgop507 <dgop507@aucklanduni.ac.nz>
2015-08-14 13:34:06 +1200

    Started implementing QualDatabase

--- onlineportfolio/src/main/java/org/se761/project/onlineportfolio/database/QualificationDatabase.java
+++ onlineportfolio/src/main/java/org/se761/project/onlineportfolio/database/QualificationDatabase.java
@@ -1,5 +1,91 @@
 package org.se761.project.onlineportfolio.database;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.se761.project.onlineportfolio.exception.DatabaseRetrievalException;
+import org.se761.project.onlineportfolio.model.AdminGroup;
+import org.se761.project.onlineportfolio.model.Qualification;
+
 public class QualificationDatabase {
+	
+	private SessionFactory sessionFactory;
+	private Session session;
+	
+	public QualificationDatabase(){
+		
+	}
+	
+	/**
+	 * Opens the current session
+	 */
+	public void openSessionFactory(){
+		Configuration c = new Configuration().configure();
+		StandardServiceRegistryBuilder builder = new StandardServiceRegistryBuilder().
+				applySettings(c.getProperties());
+
+		sessionFactory = c.buildSessionFactory(builder.build());
+	}
+	
+	/**
+	 * Closing the current session
+	 */
+	public void closeSessionFactory(){
+		sessionFactory.close();
+
+	}
+	
+	/**
+	 * Getting a particular qualification from the database
+	 */
+	public Qualification getQual(int qualId){
+		openSessionFactory();
+		session = sessionFactory.openSession();
+		Qualification qual = (Qualification) session.get(Qualification.class, qualId);
+		
+		if(qual == null){
+			closeSessionFactory();
+			throw new DatabaseRetrievalException("Qual with id " + qualId + " could not be found");
+		}
+		
+		session.close();
+		closeSessionFactory();
+		return qual;
+	}
+	
+	/**
+	 * Adding a qualification to the database
+	 */
+	public void addQul(Qualification qual){
+		openSessionFactory();
+		session = sessionFactory.openSession();
+		session.beginTransaction();
+		session.save(qual);
+		session.getTransaction().commit();
+		session.close();
+		closeSessionFactory();
+	}
+	
+	/**
+	 * Delete qualification from database
+	 */
+	public Qualification deleteAdminGroup(int qualId){
+		openSessionFactory();
+		session = sessionFactory.openSession();
+		session.beginTransaction();
+		Qualification qual = (Qualification) session.get(Qualification.class, qualId);
+		
+		if(qual == null){
+			closeSessionFactory();
+			throw new DatabaseRetrievalException("Qual with id " + qualId + " could not be found.");
+		}
+		
+		session.delete(qual);
+		session.getTransaction().commit();
+		session.close();
+		closeSessionFactory();
+		return qual;
+	}
 
 }
