7607f0771303e8322ae207a56433a23f47204d40
dgop507 <dgop507@aucklanduni.ac.nz>
2015-08-14 16:46:19 +1200

    Implemented AdminGroupService

--- onlineportfolio/src/main/java/org/se761/project/onlineportfolio/service/AdminGroupService.java
+++ onlineportfolio/src/main/java/org/se761/project/onlineportfolio/service/AdminGroupService.java
@@ -1,5 +1,34 @@
 package org.se761.project.onlineportfolio.service;
 
+import org.se761.project.onlineportfolio.database.AdminGroupDatabase;
+import org.se761.project.onlineportfolio.model.AdminGroup;
+
 public class AdminGroupService {
+	
+	private AdminGroupDatabase adminGroupDatabase = new AdminGroupDatabase();
+	
+	/**
+	 * Get a admin group from the database
+	 */
+	public AdminGroup getAdminGroup(int adminGroupId){
+		AdminGroup adminGroup = adminGroupDatabase.getAdminGroup(adminGroupId);	
+		return adminGroup;
+	}
+	
+	/**
+	 * Add a admin group to the database
+	 */
+	public void addAdminGroup(AdminGroup adminGroup){
+		adminGroupDatabase.addAdminGroup(adminGroup);
+
+	}
+	
+	/**
+	 * Delete admin group from database
+	 */
+	public AdminGroup deleteAdminGroup(int adminGroupId){
+		AdminGroup adminGroup = adminGroupDatabase.deleteAdminGroup(adminGroupId);
+		return adminGroup;
+	}
 
 }
