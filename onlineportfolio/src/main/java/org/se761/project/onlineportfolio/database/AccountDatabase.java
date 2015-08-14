eb84a99738f3ebfae78b0cf420a659af9081f04a
dgop507 <dgop507@aucklanduni.ac.nz>
2015-08-14 12:49:15 +1200

    Completed AccountDatabase implementation

--- onlineportfolio/src/main/java/org/se761/project/onlineportfolio/database/AccountDatabase.java
+++ onlineportfolio/src/main/java/org/se761/project/onlineportfolio/database/AccountDatabase.java
@@ -1,5 +1,10 @@
 package org.se761.project.onlineportfolio.database;
 
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
@@ -36,9 +41,9 @@ public class AccountDatabase {
 	}
 	
 	/**
-	 * Retrieves details for a particular account 
+	 * Gets details for a particular account 
 	 */
-	public Account getUserDetails(int accountId){
+	public Account getAccountDetails(int accountId){
 		openSessionFactory();
 		session = sessionFactory.openSession();
 		Account account = (Account) session.get(Account.class, accountId);
@@ -51,6 +56,94 @@ public class AccountDatabase {
 		session.close();
 		closeSessionFactory();
 		return account;
+	}
+	
+	/**
+	 * Gets all the accounts from the database
+	 */
+	public List<Account> getAllAccounts(){
+		openSessionFactory();
+		session = sessionFactory.openSession();
+		session.beginTransaction();
+		String getAllQuery = "FROM Account a";
+		Query query = session.createQuery(getAllQuery);
+
+		List<Account> accounts = query.list();
+		
+		if(accounts.size() == 0){
+			closeSessionFactory();
+			throw new DatabaseRetrievalException("No accounts in the database to display.");
+		}
+		
+		session.close();
+		closeSessionFactory();
+		return accounts;
+	}
+	
+	
+	/**
+	 * Gets all the client accounts from the database
+	 */
+	public List<Account> getClientAccounts(){
+		List<Account> clientAccounts = new ArrayList<Account>();
+		
+		List<Account> allAccounts = getAllAccounts();
+		for (Account a : allAccounts){
+			if(a.isAdmin() == false){
+				clientAccounts.add(a);
+			}
+		}
+		
+		return clientAccounts;
+	}
+	
+	/**
+	 * Gets all the admin accounts from the database
+	 */
+	public List<Account> getAdminAccounts(){
+		List<Account> adminAccounts = new ArrayList<Account>();
+		
+		List<Account> allAccounts = getAllAccounts();
+		for (Account a : allAccounts){
+			if(a.isAdmin() == true){
+				adminAccounts.add(a);
+			}
+		}
+		
+		return adminAccounts;
+	}
+	
+	/**
+	 * Adds an account to the database
+	 */
+	public void addAccountDetails(Account account){
+		openSessionFactory();
+		session = sessionFactory.openSession();
+		session.beginTransaction();
+		session.save(account);
+		session.getTransaction().commit();
+		session.close();
+		closeSessionFactory();
+	}
+	
+	/**
+	 * Deletes an account from the database
+	 */
+	public void removeAccount(int accountId){
+		openSessionFactory();
+		session = sessionFactory.openSession();
+		session.beginTransaction();
+		Account account = (Account) session.get(Account.class, accountId);
+		
+		if(account == null){
+			closeSessionFactory();
+			throw new DatabaseRetrievalException("Account with id " + accountId + " could not be found.");
+		}
+		
+		session.delete(account);
+		session.getTransaction().commit();
+		session.close();
+		closeSessionFactory();
 	}
 
 }
