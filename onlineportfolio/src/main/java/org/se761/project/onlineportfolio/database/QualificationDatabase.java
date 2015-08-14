18b537f619eb94f555b597ffd8e890a835662258
dgop507 <dgop507@aucklanduni.ac.nz>
2015-08-14 16:42:39 +1200

    Created AccountService

--- onlineportfolio/src/main/java/org/se761/project/onlineportfolio/database/QualificationDatabase.java
+++ onlineportfolio/src/main/java/org/se761/project/onlineportfolio/database/QualificationDatabase.java
@@ -1,10 +1,14 @@
 package org.se761.project.onlineportfolio.database;
 
+import java.util.List;
+
+import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
 import org.se761.project.onlineportfolio.exception.DatabaseRetrievalException;
+import org.se761.project.onlineportfolio.model.Account;
 import org.se761.project.onlineportfolio.model.AdminGroup;
 import org.se761.project.onlineportfolio.model.Qualification;
 
@@ -52,6 +56,29 @@ public class QualificationDatabase {
 		session.close();
 		closeSessionFactory();
 		return qual;
+	}
+	
+	
+	/**
+	 * Gets all the quals from the database
+	 */
+	public List<Qualification> getAllQuals(){
+		openSessionFactory();
+		session = sessionFactory.openSession();
+		session.beginTransaction();
+		String getAllQuery = "FROM Qualification q";
+		Query query = session.createQuery(getAllQuery);
+
+		List<Qualification> quals = query.list();
+		
+		if(quals.size() == 0){
+			closeSessionFactory();
+			throw new DatabaseRetrievalException("No accounts in the database to display.");
+		}
+		
+		session.close();
+		closeSessionFactory();
+		return quals;
 	}
 	
 	/**
--- onlineportfolio/src/main/java/org/se761/project/onlineportfolio/service/AccountService.java
+++ onlineportfolio/src/main/java/org/se761/project/onlineportfolio/service/AccountService.java
@@ -1,5 +1,60 @@
 package org.se761.project.onlineportfolio.service;
 
+
+import java.util.List;
+
+import org.se761.project.onlineportfolio.database.AccountDatabase;
+import org.se761.project.onlineportfolio.model.Account;
+
 public class AccountService {
 
+	private AccountDatabase accountDatabase = new AccountDatabase();
+	
+	/**
+	 * Gets details for a particular account 
+	 */
+	public Account getAccountDetails(int accountId){
+		Account account = accountDatabase.getAccountDetails(accountId);
+		return account;
+	}
+	
+	/**
+	 * Gets all the accounts from the database
+	 */
+	public List<Account> getAllAccounts(){
+		List<Account> accounts = accountDatabase.getAllAccounts();	
+		return accounts;
+	}
+	
+	
+	/**
+	 * Gets all the client accounts from the database
+	 */
+	public List<Account> getClientAccounts(){
+		List<Account> clientAccounts = accountDatabase.getClientAccounts();
+		return clientAccounts;
+	}
+	
+	/**
+	 * Gets all the admin accounts from the database
+	 */
+	public List<Account> getAdminAccounts(){
+		List<Account> adminAccounts = accountDatabase.getAdminAccounts();
+		return adminAccounts;
+	}
+	
+	/**
+	 * Adds an account to the database
+	 */
+	public void addAccountDetails(Account account){
+		accountDatabase.addAccountDetails(account);
+	}
+	
+	/**
+	 * Deletes an account from the database
+	 */
+	public Account removeAccount(int accountId){
+		Account account = accountDatabase.removeAccount(accountId);
+		return account;
+	}
 }
