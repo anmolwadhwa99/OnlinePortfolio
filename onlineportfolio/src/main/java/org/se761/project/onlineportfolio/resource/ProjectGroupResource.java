dad3c7b5b8dbb59b46eab21ae23ada6650ecfd70
dgop507 <dgop507@aucklanduni.ac.nz>
2015-08-23 18:39:56 +1200

    Created template for ProjectGroupResource

--- onlineportfolio/src/main/java/org/se761/project/onlineportfolio/resource/ProjectGroupResource.java
+++ onlineportfolio/src/main/java/org/se761/project/onlineportfolio/resource/ProjectGroupResource.java
@@ -1,6 +1,32 @@
 package org.se761.project.onlineportfolio.resource;
 
+import java.util.List;
+
+import org.se761.project.onlineportfolio.model.ProjectGroup;
+import org.se761.project.onlineportfolio.service.ProjectGroupService;
+
 public class ProjectGroupResource {
+	
+	private ProjectGroupService projectGroupService = new ProjectGroupService();
+	
+	public void addProjectGroup(ProjectGroup projectGroup){
+		
+	}
+	
+	public ProjectGroup getProjectGroup(int projectGroupId){
+		return null;
+		
+	}
+	
+	public List<ProjectGroup> getAllProjectGroups(){
+		return null;
+		
+	}
+	
+	public ProjectGroup deleteProjectGroup(int projectGroupId){
+		return null;
+		
+	}
 	
 	
 
