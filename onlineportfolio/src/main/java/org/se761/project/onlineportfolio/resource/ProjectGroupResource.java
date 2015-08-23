d1742ba2d384acc235816ebb23a79a11577091b8
dgop507 <dgop507@aucklanduni.ac.nz>
2015-08-23 18:44:35 +1200

    Added annotations to ProjectGroupResource

--- onlineportfolio/src/main/java/org/se761/project/onlineportfolio/resource/ProjectGroupResource.java
+++ onlineportfolio/src/main/java/org/se761/project/onlineportfolio/resource/ProjectGroupResource.java
@@ -2,29 +2,47 @@ package org.se761.project.onlineportfolio.resource;
 
 import java.util.List;
 
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
 import org.se761.project.onlineportfolio.model.ProjectGroup;
 import org.se761.project.onlineportfolio.service.ProjectGroupService;
 
+@Path("/projectGroup")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
 public class ProjectGroupResource {
 	
 	private ProjectGroupService projectGroupService = new ProjectGroupService();
 	
+	@POST
 	public void addProjectGroup(ProjectGroup projectGroup){
 		projectGroupService.addProjectGroup(projectGroup);
 		
 	}
 	
-	public ProjectGroup getProjectGroup(int projectGroupId){
+	@GET
+	@Path("/{projectGroupId}")
+	public ProjectGroup getProjectGroup(@PathParam("projectGroupId") int projectGroupId){
 		return null;
 		
 	}
 	
+	@GET
 	public List<ProjectGroup> getAllProjectGroups(){
 		return projectGroupService.getAllProjectGroups();
 		
 	}
 	
-	public ProjectGroup deleteProjectGroup(int projectGroupId){
+	@DELETE
+	@Path("/{projectGroupId}")
+	public ProjectGroup deleteProjectGroup(@PathParam("projectGroupId") int projectGroupId){
 		return projectGroupService.deleteProjGroup(projectGroupId);
 		
 	}
