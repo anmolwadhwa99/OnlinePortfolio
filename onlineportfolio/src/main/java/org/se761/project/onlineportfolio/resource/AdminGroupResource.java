1cdd6399eaa09010673fc83dea52a96d48225682
dgop507 <dgop507@aucklanduni.ac.nz>
2015-08-24 22:07:26 +1200

    Linked Editing AdminGroup with Resource layer (PUT)
    implemented PUT method

--- onlineportfolio/src/main/java/org/se761/project/onlineportfolio/resource/AdminGroupResource.java
+++ onlineportfolio/src/main/java/org/se761/project/onlineportfolio/resource/AdminGroupResource.java
@@ -6,6 +6,7 @@ import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -77,4 +78,14 @@ public class AdminGroupResource {
 	public List<AdminGroup> getAllAdminGroupsFromQualification(@PathParam("qualificationId") int qualificationId){
 		return adminGroupService.getAllAdminGroupsFromQualification(qualificationId);
 	}
+	
+	/**
+	 * Edit admin group
+	 */
+	@PUT
+	@Path("/{adminGroupId}")
+	public AdminGroup editAdminGroupDetails(@PathParam("adminGroupId") int adminGroupId, AdminGroup adminGroup){
+		return adminGroupService.editAdminGroupDetails(adminGroupId, adminGroup);
+	}
+	
 }
