a2c2875f84ec7095d0b756cadf354a405bcdfd82
anmolwadhwa99 <anmolwadhwa99@gmail.com>
2015-09-04 23:34:21 +1200

    Added all the methods to ImageService

--- onlineportfolio/src/main/java/org/se761/project/onlineportfolio/service/ImageService.java
+++ onlineportfolio/src/main/java/org/se761/project/onlineportfolio/service/ImageService.java
@@ -1,5 +1,7 @@
 package org.se761.project.onlineportfolio.service;
 
+import java.util.List;
+
 import org.se761.project.onlineportfolio.database.ImageDatabase;
 import org.se761.project.onlineportfolio.model.Image;
 
@@ -21,4 +23,38 @@ public class ImageService {
 		return imageDatabase.getImage(imageId);
 	}
 	
+	/**
+	 * Mark an image as inactive
+	 */
+	public Image deleteImage(int imageId){
+		return imageDatabase.deleteImage(imageId);
+	}
+	
+	/**
+	 * Add an image to a qualification
+	 */
+	public Image addImageToQualification(int imageId, int qualId){
+		return imageDatabase.addImageToQualification(imageId, qualId);
+	}
+	
+	/**
+	 * Get all images that are client logos
+	 */
+	public List<Image> getAllClientImages(){
+		return imageDatabase.getAllClientImages();
+	}
+	
+	/**
+	 * Get all images that are associated with a project
+	 */
+	public List<Image> getAllProjectImages(){
+		return imageDatabase.getAllProjectImages();
+	}
+	
+	/**
+	 * Get all images that are stored in the database
+	 */
+	public List<Image> getAllImages(){
+		return imageDatabase.getAllImages();
+	}
 }
