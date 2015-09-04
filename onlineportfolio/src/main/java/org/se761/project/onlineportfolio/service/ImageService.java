55bd9341fa6482b402934f65ae1f1bfb586fb01b
anmolwadhwa99 <anmolwadhwa99@gmail.com>
2015-09-04 21:45:55 +1200

    Added new class ImageService

---
+++ onlineportfolio/src/main/java/org/se761/project/onlineportfolio/service/ImageService.java
@@ -0,0 +1,10 @@
+package org.se761.project.onlineportfolio.service;
+
+import org.se761.project.onlineportfolio.database.ImageDatabase;
+
+public class ImageService {
+	
+	private ImageDatabase imageDatabase = new ImageDatabase();
+	
+	
+}
