7205bdc947a663ba11d9f53ddc6bc9aeae07748b
anmolwadhwa99 <anmolwadhwa99@gmail.com>
2015-09-04 23:45:45 +1200

    Added most of the methods to ImageResource

--- onlineportfolio/src/main/java/org/se761/project/onlineportfolio/resource/ImageResource.java
+++ onlineportfolio/src/main/java/org/se761/project/onlineportfolio/resource/ImageResource.java
@@ -1,10 +1,16 @@
 package org.se761.project.onlineportfolio.resource;
 
+import java.util.List;
+
 import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+import org.se761.project.onlineportfolio.model.Image;
 import org.se761.project.onlineportfolio.service.ImageService;
 
 
@@ -14,5 +20,49 @@ import org.se761.project.onlineportfolio.service.ImageService;
 @Produces(MediaType.APPLICATION_JSON)
 public class ImageResource {
 	private ImageService imageService = new ImageService();
+	
+	/**
+	 * Adding an image
+	 */
+	@POST
+	public Image addImage(Image image){
+		return imageService.addImage(image);
+	}
+	
+	/**
+	 * Get all images stored in the database
+	 */
+	@GET 
+	public List<Image> getAllImages(){
+		return imageService.getAllImages();
+	}
+	
+	/**
+	 * Get all client images stored in the database
+	 */
+	@GET
+	@Path("/client")
+	public List<Image> getAllClientImages(){
+		return imageService.getAllClientImages();
+	}
+	
+	/**
+	 * Get all project images stored in the database
+	 */
+	@Path("/project")
+	public List<Image> getAllProjectImages(){
+		return imageService.getAllProjectImages();
+	}
+	
+	/**
+	 * Getting any image
+	 */
+	@GET
+	@Path("/{imageId}")
+	public Image getImage(@PathParam("imageId") int imageId){
+		return imageService.getImage(imageId);
+	}
+	
+	
 	
 }
