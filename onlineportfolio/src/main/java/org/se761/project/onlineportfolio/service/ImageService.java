81e7228558ad88193114fccabd48d2a75fc2b36a
anmolwadhwa99 <anmolwadhwa99@gmail.com>
2015-09-04 21:49:19 +1200

    Added documentation to ImageService

--- onlineportfolio/src/main/java/org/se761/project/onlineportfolio/service/ImageService.java
+++ onlineportfolio/src/main/java/org/se761/project/onlineportfolio/service/ImageService.java
@@ -1,10 +1,24 @@
 package org.se761.project.onlineportfolio.service;
 
 import org.se761.project.onlineportfolio.database.ImageDatabase;
+import org.se761.project.onlineportfolio.model.Image;
 
 public class ImageService {
 	
 	private ImageDatabase imageDatabase = new ImageDatabase();
 	
+	/**
+	 * Add an image
+	 */
+	public Image addImage(Image image){
+		return imageDatabase.addImage(image);
+	}
+	
+	/**
+	 * Retrieve an image
+	 */
+	public Image getImage(int imageId){
+		return imageDatabase.getImage(imageId);
+	}
 	
 }
