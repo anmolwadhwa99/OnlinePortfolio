6f42c69266516efa09c38af5bd2a81e4b1d9c5b8
dgop507 <dgop507@aucklanduni.ac.nz>
2015-09-06 12:46:59 +1200

    Functionality to add Image to Heroku

---
+++ onlineportfolio/src/main/java/org/se761/project/onlineportfolio/heroku/ImageData.java
@@ -0,0 +1,37 @@
+package org.se761.project.onlineportfolio.heroku;
+
+
+
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.se761.project.onlineportfolio.model.Image;
+
+public class ImageData extends Server{
+	
+	public void addImage(Image image){
+		JSONObject jsonImage = new JSONObject();
+		try {
+			jsonImage.put("imageUrl", image.getImageUrl());
+			jsonImage.put("imageType", image.getImageType());
+			jsonImage.put("imageName", image.getImageName());
+			jsonImage.put("isActive", image.isActive());
+
+
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		String inputURL = SERVER_ADDRESS + IMAGE_URL;
+		Server.HTTPPostMethod(inputURL, jsonImage);
+	}
+
+	public static void main(String[] args) {
+		ImageData imageData = new ImageData();
+		Image image = new Image();
+		image.setImageName("Yolo");
+		imageData.addImage(image);
+
+	}
+
+}
