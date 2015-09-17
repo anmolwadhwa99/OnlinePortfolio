69b48a7b83ee6b60cc4eb979c88dad95de571645
Sufiyaan Mitha <mmit764@aucklanduni.ac.nz>
2015-09-17 23:08:23 +1200

    Started to clean up filmstrip, added arrow for navigating, and located places where changes need to be made

--- phonegap_website/www/filmStripSlider/jquery.flexslider.js
+++ phonegap_website/www/filmStripSlider/jquery.flexslider.js
@@ -301,7 +301,7 @@
       },
       directionNav: {
         setup: function() {
-          var directionNavScaffold = $('<ul class="' + namespace + 'direction-nav"><li class="' + namespace + 'nav-prev"><a class="' + namespace + 'prev" href="#">' + slider.vars.prevText + '</a></li><li class="' + namespace + 'nav-next"><a class="' + namespace + 'next" href="#">' + slider.vars.nextText + '</a></li></ul>');
+          var directionNavScaffold = $('<ul class="' + namespace + 'direction-nav"><li class="' + namespace + 'nav-prev"><a class="' + namespace + 'prev" href="#">' + slider.vars.prevText + '</a></li><li class="' + namespace + 'nav-next"><img src="img/arrowRight.png" class="' + namespace + 'next" href="#">' + slider.vars.nextText + '</img></li></ul>');
 
           // CUSTOM DIRECTION NAV:
           if (slider.customDirectionNav) {
