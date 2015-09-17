1fe43d4e9a43ac08c536be326c5aaaac7f893ffd
Sufiyaan Mitha <mmit764@aucklanduni.ac.nz>
2015-09-17 23:39:02 +1200

    Created a default variable for filmstrip, holds the reference to the arrow image. Also right arrow now displayed and can be clicked to navigate.

--- phonegap_website/www/filmStripSlider/jquery.flexslider.js
+++ phonegap_website/www/filmStripSlider/jquery.flexslider.js
@@ -301,7 +301,7 @@
       },
       directionNav: {
         setup: function() {
-          var directionNavScaffold = $('<ul class="' + namespace + 'direction-nav"><li class="' + namespace + 'nav-prev"><a class="' + namespace + 'prev" href="#">' + slider.vars.prevText + '</a></li><li class="' + namespace + 'nav-next"><img src="img/arrowRight.png" class="' + namespace + 'next" href="#">' + slider.vars.nextText + '</img></li></ul>');
+          var directionNavScaffold = $('<ul class="' + namespace + 'direction-nav"><li class="' + namespace + 'nav-prev"><a class="' + namespace + 'prev" href="#">' + slider.vars.prevText + '</a></li><li class="' + namespace + 'nav-next"><a class="' + namespace + 'next" href="#">' + slider.vars.rightArrow + '</a></li></ul>');
 
           // CUSTOM DIRECTION NAV:
           if (slider.customDirectionNav) {
@@ -1125,6 +1125,7 @@
     directionNav: true,             //Boolean: Create navigation for previous/next navigation? (true/false)
     prevText: "Previous",           //String: Set the text for the "previous" directionNav item
     nextText: "Next",               //String: Set the text for the "next" directionNav item
+    rightArrow: "<img src=\"img/arrowRight.png\" width = \"35px\" height = \"35px\">",
 
     // Secondary Navigation
     keyboard: true,                 //Boolean: Allow slider navigating via keyboard left/right keys
