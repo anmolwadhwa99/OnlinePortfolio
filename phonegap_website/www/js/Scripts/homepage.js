e68c40a80cd6b007071ceb3e5ba073e8c43b429b
Juhi Kamal Goswami <jgos311@aucklanduni.ac.nz>
2015-09-20 22:34:24 +1200

    Fixed the issue of the "qual added to cart" notification permanently being present at the top of the page

--- phonegap_website/www/js/Scripts/homepage.js
+++ phonegap_website/www/js/Scripts/homepage.js
@@ -387,6 +387,7 @@ function addToCart(qID, m){
 
     if(qID != null) {
         document.getElementById("divAddToCartAlert").style.display = 'block';
+
         document.getElementById("addToCartOkButton").addEventListener("click", function () {
             document.getElementById("divAddToCartAlert").style.display = 'none';
         });
