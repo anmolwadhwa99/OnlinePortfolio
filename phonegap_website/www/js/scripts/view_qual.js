262609668144792d3765d8c5c47678ed88461104
dgop507 <dgop507@aucklanduni.ac.nz>
2015-09-20 16:58:58 +1200

    Duplicate button now takes you to Add Qual page

--- phonegap_website/www/js/scripts/view_qual.js
+++ phonegap_website/www/js/scripts/view_qual.js
@@ -1,3 +1,9 @@
 /**
  * Created by Dheeraj on 20/09/15.
  */
+
+function duplicateQual(qual_id){
+    console.log(qual_id);
+    sessionStorage.setItem("qual_id", qual_id);
+    location.href = 'qual_add.html';
+}
