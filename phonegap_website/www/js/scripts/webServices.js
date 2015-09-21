e9aa12c278462699d011523a110effc6bd4e0072
Juhi Kamal Goswami <jgos311@aucklanduni.ac.nz>
2015-09-21 13:51:33 +1200

    Added alert message for unable to get all images

--- phonegap_website/www/js/scripts/webServices.js
+++ phonegap_website/www/js/scripts/webServices.js
@@ -1520,6 +1520,8 @@ function getAllImages(callback) {
         req.onreadystatechange = function(){
             if (req.readyState != 4) return;
             if (req.status != 200) {
+                alert_type = 'error';
+                alert("Unable to get all images");
                 return null;
             }
             // Request successful, read the response
