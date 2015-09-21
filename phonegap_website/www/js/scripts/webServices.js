f325f29a404ca08289817fd5859b513630cc5f2a
Juhi Kamal Goswami <jgos311@aucklanduni.ac.nz>
2015-09-21 13:30:34 +1200

    Added alert message for get all admin

--- phonegap_website/www/js/scripts/webServices.js
+++ phonegap_website/www/js/scripts/webServices.js
@@ -768,6 +768,8 @@ function getAllAdmins(callback){
         req.onreadystatechange = function(){
             if (req.readyState != 4) return;
             if (req.status != 200) {
+                alert_type = 'error';
+                alert("Unable to get all admins");
                 return null;
             }
             // Request successful, read the response
