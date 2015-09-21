3edf2fc24aa6ec3772b42bc3f66161463b565f1e
Juhi Kamal Goswami <jgos311@aucklanduni.ac.nz>
2015-09-21 13:45:06 +1200

    Added alert message for unable to get admin group by account

--- phonegap_website/www/js/scripts/webServices.js
+++ phonegap_website/www/js/scripts/webServices.js
@@ -1247,6 +1247,8 @@ function getAdminGroupsByAccount(acId, callback){
         req.onreadystatechange = function(){
             if (req.readyState != 4) return;
             if (req.status != 200) {
+                alert_type = 'error';
+                alert("Unable to get admin group by account");
                 return null;
             }
             // Request successful, read the response
