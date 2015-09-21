440833bb9929c73edb17529ac499dbc89bdb3bdb
Juhi Kamal Goswami <jgos311@aucklanduni.ac.nz>
2015-09-21 13:39:37 +1200

    Added alert message for unable to get accounts by project group

--- phonegap_website/www/js/scripts/webServices.js
+++ phonegap_website/www/js/scripts/webServices.js
@@ -1047,6 +1047,8 @@ function getAccountsByProjectGroup(pgId, callback){
         req.onreadystatechange = function(){
             if (req.readyState != 4) return;
             if (req.status != 200) {
+                alert_type = 'error';
+                alert("Unable to get accounts by project group");
                 return null;
             }
             // Request successful, read the response
