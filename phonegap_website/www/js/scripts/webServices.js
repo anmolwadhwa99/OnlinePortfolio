9c6a9f6ee48d71a35ec42b10c1d7487f89062909
Juhi Kamal Goswami <jgos311@aucklanduni.ac.nz>
2015-09-21 13:42:57 +1200

    Added alert message for unable to delete admin group

--- phonegap_website/www/js/scripts/webServices.js
+++ phonegap_website/www/js/scripts/webServices.js
@@ -1186,6 +1186,8 @@ function deleteAdminGroup(id){
         req.onreadystatechange = function(){
             if (req.readyState != 4) return;
             if (req.status != 200) {
+                alert_type = 'error';
+                alert("Unable to delete admin group");
                 return null;
             }
             // Request successful, read the response
