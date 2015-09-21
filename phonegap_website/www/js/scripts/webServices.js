398856b25afa35e5cb97f8da9293bac2fbb77336
Juhi Kamal Goswami <jgos311@aucklanduni.ac.nz>
2015-09-21 13:08:50 +1200

    Added alert message wrapper in homepage.html

--- phonegap_website/www/js/scripts/webServices.js
+++ phonegap_website/www/js/scripts/webServices.js
@@ -280,6 +280,8 @@ function getAllQuals(callback){
         req.onreadystatechange = function(){
             if (req.readyState != 4) return;
             if (req.status != 200) {
+                alert_type = 'error';
+                alert("Unable to return Qual");
                 return null;
             }
             // Request successful, read the response
