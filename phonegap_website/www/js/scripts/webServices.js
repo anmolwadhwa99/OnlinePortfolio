8717a3ff9e44eafb1209df8bb91217b69a5ffa50
Juhi Kamal Goswami <jgos311@aucklanduni.ac.nz>
2015-09-21 13:52:40 +1200

    Added alert message for unable to get all project images

--- phonegap_website/www/js/scripts/webServices.js
+++ phonegap_website/www/js/scripts/webServices.js
@@ -1588,6 +1588,8 @@ function getAllProjectImages(callback) {
         req.onreadystatechange = function(){
             if (req.readyState != 4) return;
             if (req.status != 200) {
+                alert_type = 'error';
+                alert("Unable to get all project images");
                 return null;
             }
             // Request successful, read the response
