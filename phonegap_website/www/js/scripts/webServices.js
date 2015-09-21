95c45fba1932ad0d5f523a75a8084da7ed33e08d
Juhi Kamal Goswami <jgos311@aucklanduni.ac.nz>
2015-09-21 13:46:50 +1200

    Added alert message for when unable to get project by id

--- phonegap_website/www/js/scripts/webServices.js
+++ phonegap_website/www/js/scripts/webServices.js
@@ -1322,6 +1322,8 @@ function getProjectById(id, callback){
         req.onreadystatechange = function(){
             if (req.readyState != 4) return;
             if (req.status != 200) {
+                alert_type = 'error';
+                alert("Unable to get project by id");
                 return null;
             }
             // Request successful, read the response
