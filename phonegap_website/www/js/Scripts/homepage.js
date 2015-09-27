12ab446d0f22ef44f5ac5102575005e8ee4e2a94
Tazt <thus056@aucklanduni.ac.nz>
2015-09-28 00:19:34 +1300

    removed duplicate from filmstrip view qual + fixed the indentation of qual cart items

--- phonegap_website/www/js/Scripts/homepage.js
+++ phonegap_website/www/js/Scripts/homepage.js
@@ -648,23 +648,57 @@ function getProjectforClient(id, clientName){
 function addToCart(qID, m){
 
     qualsToAdd.push(qID);
-    var ul = document.getElementById("ProjectCart");
-    var li = document.createElement("li");
+
+    var root = document.getElementById("ProjectCart");
+    var div = document.createElement("div");
+    var spanDiv = document.createElement("div");
+    var textDiv = document.createElement("div");
+    div.setAttribute('class', 'qual');
+    spanDiv.setAttribute('class', 'col-md-1');
+    textDiv.setAttribute('class', 'col-md-10');
+
     var a = document.createElement("a");
     a.setAttribute("href", "#");
-    li.id = "deleteQual";
+    div.id = "deleteQual";
+
+//
+//<a href="#">
+//    <div class="col-md-1"><span class="icon"></span></div>
+//    <div class="col-md-10">Scaling service across the globe</div>
+//    </a>
 
     var span = document.createElement("span");
     span.setAttribute("class","icon");
 
-    a.appendChild(span);
-    a.appendChild(document.createTextNode(m));
+    spanDiv.appendChild(span);
+    textDiv.appendChild(document.createTextNode(m));
 
 
-    li.appendChild(a);
-    li.onclick = function() {this.parentNode.removeChild(this); qualsToAdd.splice(qualsToAdd.indexOf(this),1);}
-    li.setAttribute("id", m); // added line
-    ul.appendChild(li);
+    a.appendChild(spanDiv);
+    a.appendChild(textDiv);
+    div.appendChild(a);
+    div.onclick = function() {this.parentNode.removeChild(this); qualsToAdd.splice(qualsToAdd.indexOf(this),1);}
+    div.setAttribute("id", m); // added line
+    root.appendChild(div);
+
+
+    //var ul = document.getElementById("ProjectCart");
+    //var li = document.createElement("li");
+    //var a = document.createElement("a");
+    //a.setAttribute("href", "#");
+    //li.id = "deleteQual";
+    //
+    //var span = document.createElement("span");
+    //span.setAttribute("class","icon");
+    //
+    //a.appendChild(span);
+    //a.appendChild(document.createTextNode(m));
+    //
+    //
+    //li.appendChild(a);
+    //li.onclick = function() {this.parentNode.removeChild(this); qualsToAdd.splice(qualsToAdd.indexOf(this),1);}
+    //li.setAttribute("id", m); // added line
+    //ul.appendChild(li);
 
     // displaying notification to user that qual has been added to cart
 
