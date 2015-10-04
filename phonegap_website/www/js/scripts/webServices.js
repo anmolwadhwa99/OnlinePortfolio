00a59586fa2ba8382b9cd7ce5825dfd49fa55fc2
anmolwadhwa99 <anmolwadhwa99@gmail.com>
2015-10-05 03:30:45 +1300

    Made changes to webservices.js

--- phonegap_website/www/js/scripts/webServices.js
+++ phonegap_website/www/js/scripts/webServices.js
@@ -1,6 +1,7 @@
 
-//var url = "http://onlineportfolio.herokuapp.com/webapi";
-var url = "http://localhost:8080/onlineportfolio/webapi";
+var url = "http://onlineportfolio.herokuapp.com/webapi";
+//var url = "http://localhost:8080/onlineportfolio/webapi";
+var url = "http://q-hub.herokuapp.com/webapi"
 var _qual = "/qual";
 var _ac = "/account";
 var _ag = "/admin";
@@ -711,6 +712,7 @@ function assignQualToAdminGroup(agId, qId){
 
             if (typeof callback == 'function'){
                 callback.apply(qual.id);
+                alert('Added qual to admin group');
             }
 
         }
